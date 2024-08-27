86da6380e00b2cb6f5a82c37f750d3df0c9d1b49
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-08-27 16:11:37 +0500

    changed

--- src/js/about.js
+++ src/js/about.js
@@ -8,8 +8,9 @@ import { createCountryInfo } from "./updateUI";
 const quaryString = window.location.search;
 const urlParams = new URLSearchParams(quaryString);
 const country = urlParams.get("country");
-const countryAPI = `https://restcountries.com/v3.1/${country}`;
-// console.log(countryAPI);
+
+const countryAPI = `https://restcountries.com/v3.1${country}`;
+console.log(countryAPI);
 
 request(countryAPI)
   .then((data) => {
